0f180027485502f25051b801c25293eb498256c3
Peter Kretschmann <peter.kretschmann@hotmail.de>
2012-05-30 14:26:50 +0200

    php funktion zur anzeige aller kunden in db

--- list.php
+++ list.php
@@ -19,23 +19,13 @@
 
         <div>
                 <?php
-                         $dbhandle = sqlite_open('sqlite/database', 0666, $error);
-                         if (!$dbhandle) die (§error);
-                         $query = "SELECT name, vorname FROM kunde";
-                         $result = sqlite_query($dbhandle, $query);
-                         if (!$result) die("Cannot execute query.");
-                         $row=sqlite-fetch-array($result, SQLITE_ASSOC);
-                         print_r($row);
-                         echo "<br />";
-                         sqlite_rewind($result);
-                         $row=sqlite_fetch-array($result, SQLITE_NUM);
-                         print-r($row);
-                         echo "<br />";
-                         sqlite-rewind($result);
-                         $row=sqlite_fetch_array($result, SQLITE_BOTH);
-                         print_r($row);
-                         echo "<br />";
-                         sqlite_close($dbhandle);
+                         $dir = 'sqlite:sqlite/database.db';
+                         $dbh = new PDO($dir) or die ("cannot open database");
+                         $query = "SELECT * FROM kunde";
+                         foreach ($dbh->query($query) as $row)
+                         {
+                                 echo $row[2];echo " ";echo $row[1];echo "<br />" ;
+                         }
                 ?>
         </div>
 
